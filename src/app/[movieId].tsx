4e06570132c446af62d2c734eabebe7f8f19f96c
Carlos Ival <carlos.martinez@evvohome.com>
2024-03-15 08:56:08 +0100

    Remove Token

--- src/app/[movieId].tsx
+++ src/app/[movieId].tsx
@@ -11,11 +11,12 @@ import { endEvent } from 'react-native/Libraries/Performance/Systrace';
 import Loading from '@/components/Loading';
 import { fetchMoviesDetails, image500 } from '../api/moviedb';
 import YoutubePlayer from "react-native-youtube-iframe";
-
+import  { IMovie } from '../core/movieType';
 
 const { width, height } = Dimensions.get('window');
 const ios = Platform.OS == 'ios';
 const topMargin = ios ? '' : 'mt-3'
+
 
 export default function MovieDetails() {
   const navigation = useNavigation();
@@ -23,7 +24,7 @@ export default function MovieDetails() {
   const [isFavourite, setisFavorite] = useState(false);
   const [loading, setLoading] = useState(true);
   const { movieId } = useLocalSearchParams<{ movieId: string }>();
-  const [movie, setMovie] = useState()
+  const [movie, setMovie] = useState<IMovie>()
   const [playing, setPlaying] = useState(false);
 
   useLayoutEffect(() => {
@@ -35,15 +36,15 @@ export default function MovieDetails() {
   }, []);
 
   useEffect(() => {
-     // call the movie details api
-     console.log("movieId: ", movieId)
-     setLoading(true);
-     getMovieDetails(movieId);
+    // call the movie details api
+    console.log("movieId: ", movieId)
+    setLoading(true);
+    getMovieDetails(movieId);
   }, [movieId])
 
-  const getMovieDetails = async movieId =>{
+  const getMovieDetails = async movieId => {
     const data = await fetchMoviesDetails(movieId);
-    if(data){ 
+    if (data) {
       // add static uri video
       setMovie(data);
     }
@@ -61,11 +62,11 @@ export default function MovieDetails() {
     setPlaying((prev) => !prev);
   }, []);
 
-  const isyoutubeVideo = (() => movie?.videos?.results?.filter(item=>item.site == "YouTube").length > 0)()
-  
+  const isyoutubeVideo = (() => movie?.videos?.results?.filter(item => item.site == "YouTube").length > 0)()
 
-  const getYoutubeVideoKey = ()=>{
-    return  movie?.videos?.results?.filter(item=>item.site == "YouTube")[0].key //'iee2TATGMyI'
+
+  const getYoutubeVideoKey = () => {
+    return movie?.videos?.results?.filter(item => item.site == "YouTube")[0].key //'iee2TATGMyI'
   }
 
 
@@ -77,54 +78,54 @@ export default function MovieDetails() {
       {/* back button and movie*/}
       <View className="w-full">
         <SafeAreaView className={"absolute z-20 w-full flex-row flex-wrap px-4 "}>
-        <View className='flex-row flex-wrap justify-between  my-8'>
-          <TouchableOpacity  style={{backgroundColor:theme.background}} onPress={() => router.back()} className="rounded-xl p-1 ml-4">
-            <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity  onPress={() => setisFavorite(!isFavourite)} className="p-1 mx-4">
-            <HeartIcon size="35" color={isFavourite ? theme.background : "white"}></HeartIcon>
-          </TouchableOpacity>
-        </View>      
+          <View className='flex-row flex-wrap justify-between my-8'>
+            <TouchableOpacity style={{ backgroundColor: theme.background }} onPress={() => router.back()} className="rounded-xl p-1 ml-4">
+              <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setisFavorite(!isFavourite)} className="p-1 mx-4">
+              <HeartIcon size="35" color={isFavourite ? theme.background : "white"}></HeartIcon>
+            </TouchableOpacity>
+          </View>
         </SafeAreaView>
         {
           loading ? (
             <Loading />
           ) : (
             <View>
-              {isyoutubeVideo? (<YoutubePlayer
-        height={height*0.30}
-        play={playing}
-        videoId={getYoutubeVideoKey()}
-        onChangeState={onStateChange}
-      />):(<Image
-        source={{uri:image500(movie?.poster_path)}}
-        style={{ width, height: height*0.55 }}
-      />)}
-              
-              
-              
+              {isyoutubeVideo ? (<YoutubePlayer
+                height={height * 0.30}
+                play={playing}
+                videoId={getYoutubeVideoKey()}
+                onChangeState={onStateChange}
+              />) : (<Image
+                source={{ uri: image500(movie?.poster_path) }}
+                style={{ width, height: height * 0.55 }}
+              />)}
+
+
+
               {/* movie details*/}
               <View className="space-y-3">
                 <Text className="text-white text-center text-3xl font-bold tracking-wider m-4">
-                  { movie?.title}
+                  {movie?.title}
                 </Text>
                 <Text className='text-neutral-400 font-semibold text-base text-center'>
                   {movie?.status} · {movie?.release_date.split('-')[0]} · {movie?.runtime} min
                 </Text>
                 <View className='w-full flex-row justify-center'>
-                  { isyoutubeVideo && (
-                  <TouchableOpacity onPress={togglePlaying}>
-                  {playing?(<PauseIcon size="45" strokeWidth={2.5} color={theme.color}></PauseIcon>):(<PlayIcon size="45" strokeWidth={2.5} color={theme.color}></PlayIcon>)}
-                  </TouchableOpacity>
-                  )} 
+                  {isyoutubeVideo && (
+                    <TouchableOpacity onPress={togglePlaying}>
+                      {playing ? (<PauseIcon size="45" strokeWidth={2.5} color={theme.color}></PauseIcon>) : (<PlayIcon size="45" strokeWidth={2.5} color={theme.color}></PlayIcon>)}
+                    </TouchableOpacity>
+                  )}
                 </View>
                 <View className="flex-row justify-center mx-4 space-x-2">
                   {
-                    movie?.genres?.map((genre, idx)=>{
-                      let showDot = idx+1 != movie?.genres.length;
+                    movie?.genres?.map((genre, idx) => {
+                      let showDot = idx + 1 != movie?.genres.length;
                       return (
-                        <Text key={idx} className='text-neutral-400 font-semibold text-base text-center'> 
-                        {genre?.name} {showDot && " · "}
+                        <Text key={idx} className='text-neutral-400 font-semibold text-base text-center'>
+                          {genre?.name} {showDot && " · "}
                         </Text>
 
                       )
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,2 +1,2 @@
-export const apiKey = '2876006dfd105e09b7e019f16f47620e'
-export const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODc2MDA2ZGZkMTA1ZTA5YjdlMDE5ZjE2ZjQ3NjIwZSIsInN1YiI6IjY1ZTdlOWViZWIwOTMyMDE4NzFiY2NmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L5hZAsFnePd1A38Orz-rOjsD3zMxbXbnXZxIq3CEr4A'
+export const apiKey = ''
+export const token = ''
